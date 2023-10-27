b007921a4996235927a0ce2fe436ed7e1b35f38e
DaiC-S <yuangudazhi@gmail.com>
2023-10-27 10:34:32 +0900

    createアクションのルーティング設定
    # Why
    
    ・form_withのリクエストに対応するため。
    
    # What
    
    ・routes.rbのresourcesメソッドに:createを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "vouchers#index"
-  resources :vouchers, only: [:index, :new]
+  resources :vouchers, only: [:index, :new, :create]
 end
