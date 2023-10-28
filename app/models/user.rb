5192610f8f3ec4ab5c465687463b56a4852bc55f
DaiC-S <yuangudazhi@gmail.com>
2023-10-28 12:18:26 +0900

    Userモデルへのバリデーションの記述
    # Why
    ・マイページのURLに含める識別文字列を、ユーザーIDではなくユーザー名にしたいから。
    ⤴︎
    ・半角英数字以外のユーザー名を登録できないようにするため。
    
    # What
    
    ・「ユーザー名が空だと保存できない」というvalidationを設定した。
    
    ・ユーザー名のvalidationに「半角英数字のみ保存可能」の正規表現をした。
    
    ・アソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :vouchers, dependent: :destroy
+
+  with_options presence: true do
+    validates :user_name, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  end
 end
