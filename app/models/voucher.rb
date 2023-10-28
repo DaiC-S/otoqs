61aacefd3ff26394a59bcaaffd1bf906c2386e95
DaiC-S <yuangudazhi@gmail.com>
2023-10-28 12:25:18 +0900

    Voucherモデル アソシエーション・バリデーション 記述
    # What
    
    ・Not null制約をかけたカラムにvalidationを設定した。
    
    ・「コードかURLのどちらか一方のみを入力しないと保存されない」というvalidationをかけるための変数を定義した。
    
    ・Userモデルとのアソシエーションの記述

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -1,2 +1,21 @@
 class Voucher < ApplicationRecord
+
+  belongs_to :user
+
+  with_options presence: true do
+    validates :title
+    validates :status
+    validates :title
+  end
+
+  validate :required_either_code_text_or_code_url
+
+  private
+
+  def required_either_code_text_or_code_url
+    return if code_text.present? ^ code_url.present?
+
+    errors.add(:base, 'コードまたはURLのどちらか一方のみを入力してください')
+  end
+
 end
