a928ce1dc2133d29ba61b07e2995543522ea6fab
DaiC-S <yuangudazhi@gmail.com>
2023-11-07 21:03:28 +0900

    ユーザー管理 詳細表示機能
    # What
    
    ・ユーザー詳細ページへの遷移の実装。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def show
+    user = User.find_by(user_name: params[:id])
+
+    if user
+      @user_name = user.user_name
+      @vouchers = user.vouchers
+    else
+      # ユーザーが見つからない場合の処理
+      redirect_to root_path, alert: 'User not found'
+    end
   end
 end
