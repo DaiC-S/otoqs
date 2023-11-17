814846181493b6f6b5c0146be43fcf981573c76b
DaiC-S <yuangudazhi@gmail.com>
2023-11-17 15:37:48 +0900

    ユーザー管理 validation 追加
    # Why
    
    ・「限定公開」に設定したトクテンの一覧ページの末尾URLを英数字で簡潔に表すため（日本語だと長くなる）。
    
    # What
    
    ・大文字小文字を区別せずにuser_nameの一意性を検証するvalidationを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :vouchers, dependent: :destroy
 
   with_options presence: true do
-    validates :user_name, format: { with: /\A[a-zA-Z0-9]+\z/ }
+    validates :user_name, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]+\z/ }
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   end
 end
