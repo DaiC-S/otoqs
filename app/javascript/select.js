5c61ba995fc07980ae682ed5dd9fcfca143ee184
DaiC-S <yuangudazhi@gmail.com>
2023-12-06 14:48:48 +0900

    トクテン 詳細表示 編集・削除ボタン
    # What
    
    ・「・」× 3のボタンを押してドロップダウンメニューを出したとき、「・」× 3のボタン以外の背景部分の要素を押しても戻るように、jsファイルの記述を変更した。

--- app/javascript/select.js
+++ app/javascript/select.js
@@ -1,11 +1,15 @@
 window.addEventListener("turbo:load", function () {
   const pullDownButton = document.getElementById("lists");
   const pullDownParents = document.getElementById("pullDown");
-  pullDownButton.addEventListener("click", function () {
-    if (pullDownParents.style.display == "none") {
-      pullDownParents.style.display = "block";
-      document.body.style.backgroundColor = "rgba(0, 0, 0, .3)";
-    } else {
+  pullDownButton.addEventListener("click", function (e) {
+    const isDisplayed = pullDownParents.style.display === "block";
+    pullDownParents.style.display = isDisplayed ? "none" : "block";
+    document.body.style.backgroundColor = isDisplayed ? "white" : "rgba(0, 0, 0, .3)";
+    e.stopPropagation();
+  });
+
+  document.body.addEventListener("click", () => {
+    if (pullDownParents.style.display === "block") {
       pullDownParents.style.display = "none";
       document.body.style.backgroundColor = "white";
     }
