0910078cfb15cfa702ffb2cf825dbb0e0ef97067
DaiC-S <yuangudazhi@gmail.com>
2023-12-07 10:00:43 +0900

    トクテン 詳細表示 クリップボード
    #  What
    
    ・トクテンのコードまたはURLを、クリップボードアイコンを1回押すだけでクリップボードへコピーできるようにした。

---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static values = { content: String };
+
+  connect() {
+    this.originalContent = this.element.innerHTML; // 元のHTMLを保持
+  }
+
+  copy() {
+    navigator.clipboard
+      .writeText(this.contentValue)
+      .then(() => {
+        // コピー成功時の処理
+        this.element.textContent = "コピー完了🎉";
+        setTimeout(() => {
+          this.element.innerHTML = this.originalContent; // 2秒後に元のHTMLに戻す
+        }, 1000);
+      })
+      .catch(() => {
+        // コピー失敗時の処理
+        alert("🥲コピーできませんでした。");
+      });
+  }
+}
