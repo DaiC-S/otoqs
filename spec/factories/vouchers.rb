67126cd35f1e9a48224e74002b3db684ff32a099
DaiC-S <yuangudazhi@gmail.com>
2023-12-17 14:24:27 +0900

    コントローラー 単体テスト

--- spec/factories/vouchers.rb
+++ spec/factories/vouchers.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :voucher do
     title {'サンプル1'}
     code_text {'Aa1Aa1'}
-    code_url {'https://Bb2Bb2.com'}
+    code_url {nil}
     status {1}
     shared_benefit {'1,000円、キャッシュバック'}
     shared_action {'新規会員登録時、招待コードを入力'}
