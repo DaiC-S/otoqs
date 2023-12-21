7d9455ff3a90d4a72601dfccdcb36d421f4fad4a
DaiC-S <yuangudazhi@gmail.com>
2023-12-21 21:08:56 +0900

    コントローラー 単体テスト
    # Why
    
    ・テスト中にユーザーを認証する
    
    # What
    
    ・テストで、ユーザーをサインインさせてからリクエストを送信するように修正

--- spec/requests/vouchers_spec.rb
+++ spec/requests/vouchers_spec.rb
@@ -3,12 +3,15 @@ require 'rails_helper'
 RSpec.describe "Vouchers", type: :request do
 
   before do
-    @voucher = FactoryBot.create(:voucher)
+    @user = FactoryBot.create(:user)
+    @voucher = FactoryBot.create(:voucher, user: @user)
+    sign_in @user
   end
 
   describe 'GET #index' do
     it "indexアクションにリクエストすると正常にレスポンスが返ってくる" do
       get root_path
+      expect(response.status).to eq 200
     end
   end
 
