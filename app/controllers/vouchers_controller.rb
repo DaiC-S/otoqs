b0c38c8350e6ff38ef2c12f0c28be535c1071c4d
DaiC-S <yuangudazhi@gmail.com>
2023-12-23 13:52:47 +0900

    リダイレクト処理 追加
    # Why
    
    ・存在しないトクテンのデータへアクセスしようとする行為（URLを直接打ち込む等）がおこなわれた場合、トップページにリダイレクトさせるため。

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -1,7 +1,8 @@
 class VouchersController < ApplicationController
   before_action :authenticate_user!
+  before_action :not_existing_voucher, only: [:show, :edit]
   before_action :set_voucher, only: [:show, :destroy, :edit, :update]
-  before_action :redirect_top, only: [:show, :edit]
+  before_action :user_matched, only: [:show, :edit]
 
   def index
     @vouchers = Voucher.order("created_at DESC")
@@ -52,14 +53,19 @@ class VouchersController < ApplicationController
     params.require(:voucher).permit(:title, :code_text, :code_url, :status, :info, :date, :position, :shared_benefit, :shared_action).merge(user_id: current_user.id)
   end
 
-  def set_voucher
-    @voucher = Voucher.find(params[:id])
-  end
-
-  def redirect_top
-    if current_user.id != @voucher.user_id
+  def not_existing_voucher
+    unless Voucher.find_by(id: params[:id]).present?
       redirect_to '/'
     end
   end
 
+  def set_voucher
+    @voucher = Voucher.find(params[:id])
+  end
+
+  def user_matched
+    if current_user.id != @voucher.user_id
+      redirect_to '/'
+    end
+  end
 end
