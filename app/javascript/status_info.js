fba75778d87cd6da5f629df2b115759354c25799
DaiC-S <yuangudazhi@gmail.com>
2023-12-26 15:35:52 +0900

    モーダル 背景 固定
    # Why
    
    ・モーダルを表示させたときに、背景部分が操作できない（スクロールできない）ようにするため。

--- app/javascript/status_info.js
+++ app/javascript/status_info.js
@@ -2,15 +2,21 @@ function click() {
   const statusDescOpen = document.getElementById("statusDescOpen");
   const statusDescClose = document.querySelector(".statusDescClose");
   const modal = document.getElementById("statusDescModal");
+
   statusDescOpen.addEventListener("click", () => {
     modal.style.display = "block";
+    document.body.style.overflow = "hidden"; // スクロール固定を追加
   });
+
   statusDescClose.addEventListener("click", () => {
     modal.style.display = "none";
+    document.body.style.overflow = ""; // スクロール固定を解除
   });
+
   document.addEventListener("click", (e) => {
     if (e.target == statusDescModal) {
       modal.style.display = "none";
+      document.body.style.overflow = ""; // スクロール固定を解除
     }
   });
 }
